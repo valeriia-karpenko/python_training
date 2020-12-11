dba4187d62a28d7a470c52cf7c8d515c7388d422
vkarpenko <valeriia.karpenko@billing.ru>
2020-12-11 16:56:31 +0300

    Добавлены тесты на создание контактов

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, company, address):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
