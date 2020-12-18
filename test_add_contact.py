0a00aa3b5ec23b78d542a2a40a544672835cc925
vkarpenko <valeriia.karpenko@billing.ru>
2020-12-18 10:41:20 +0300

    Реализация фикстуры

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,79 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
-
+import pytest
+from application import Application
 from contact import Contact
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contacts_page(wd)
-        self.create_contact(wd, Contact(firstname="gfdgdfgdfg", middlename="sdfdfsdfsd", lastname="dfsdfsdf",
-                                        nickname="sdfdsfs", company="dfsdfds", address="fsdf"))
-        self.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contacts_page(wd)
-        self.create_contact(wd, Contact(firstname="", middlename="", lastname="",
-                                        nickname="", company="", address=""))
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_contact(self, wd, contact):
-        # init contact creation
-        wd.find_element_by_name("theform").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.middlename)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
-
-    def open_contacts_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/edit.php")
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.open_contacts_page()
+    app.create_contact(Contact(firstname="gfdgdfgdfg", middlename="sdfdfsdfsd", lastname="dfsdfsdf",
+                               nickname="sdfdsfs", company="dfsdfds", address="fsdf"))
+    app.logout()
+
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.open_contacts_page()
+    app.create_contact(Contact(firstname="", middlename="", lastname="",
+                               nickname="", company="", address=""))
+    app.logout()
