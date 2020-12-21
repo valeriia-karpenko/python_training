2bda4749384dd5cf3b84f41e6f9540e8935e05ad
vkarpenko <valeriia.karpenko@billing.ru>
2020-12-21 10:04:45 +0300

    общая фикстура

--- fixture/application.py
+++ fixture/application.py
@@ -9,14 +9,14 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
