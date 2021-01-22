8ee99bca705a0017def4c829339ebcbad5e556b6
vkarpenko <valeriia.karpenko@billing.ru>
2021-01-22 15:33:01 +0300

    Задание 15

--- fixture/session.py
+++ fixture/session.py
@@ -26,7 +26,7 @@ class SessionHelper:
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_link_text("Logout")) > 0
+        return len(wd.find_elements_by_link_text('Logout')) > 0
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
