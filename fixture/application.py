96d855604610a48c719e9e6c1c2b2635140ef496
vkarpenko <valeriia.karpenko@billing.ru>
2021-01-22 15:33:01 +0300

    Задание 16

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
@@ -7,11 +6,20 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecornized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,8 +30,9 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("Number of results:")) > 0):
-            wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
+        # if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("Number of results:")) > 0):
+        #     wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
