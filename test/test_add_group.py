e2d46d1d57796c06de4e4eb36f24192351552bdf
vkarpenko <valeriia.karpenko@billing.ru>
2021-01-25 16:13:22 +0300

    Задание 15 правки

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,17 +5,11 @@ import pytest
 from model.group import Group
 
 
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-
-testdata = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
