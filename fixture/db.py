5e1057dcd3004eff660d232331b731b2a64e5291
vkarpenko <valeriia.karpenko@billing.ru>
2021-02-07 16:41:56 +0300

    Задание 21

--- fixture/db.py
+++ fixture/db.py
@@ -30,10 +30,13 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated = '0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3, "
+                           "phone2 from addressbook where deprecated = '0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, homephone=home,
+                                    mobilephone=mobile, workphone=work, email=email, email2=email2, email3=email3,
+                                    secondaryphone=phone2))
         finally:
             cursor.close()
         return list
--- test/test_phone.py
+++ test/test_phone.py
@@ -1,5 +1,7 @@
 import re
 
+from model.contact import Contact
+
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
@@ -16,10 +18,37 @@ def test_phones_on_view_page(app):
     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
+def test_contacts_info_on_home_page_vs_db(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="firstname1", lastname="lastname1", homephone="781236223",
+                                   email="ivanov@gmail.com", mobilephone="+79119685588", workphone="88124568995",
+                                   secondaryphone="88124568988", address="Spb Nevskiy 36-89", email2="ivanov@mail.ru",
+                                   email3="ivanov@ya.ru"))
+    for index in range(len(db.get_contact_list())):
+        contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)[index]
+        contact_from_db = db.get_contact_list()[index]
+        assert clear(contact_from_home_page.firstname) == clear(contact_from_db.firstname)
+        assert clear(contact_from_home_page.lastname) == clear(contact_from_db.lastname)
+        assert clear(contact_from_home_page.address) == clear(contact_from_db.address)
+        assert clear(contact_from_home_page.all_phones_from_home_page) == merge_phones_like_on_home_page(
+            contact_from_db)
+        assert clear(contact_from_home_page.all_email_from_home_page) == merge_emails_like_from_home_page(
+            contact_from_db)
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                     map(lambda x: clear(x), filter(lambda x: x is not None, [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+                            map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                           [contact.homephone, contact.mobilephone,
+                                                            contact.workphone, contact.secondaryphone]))))
+
+
+def merge_emails_like_from_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3]))))
