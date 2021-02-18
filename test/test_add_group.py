231bcd01b121559475f1bebc107b6275b7bebf8c
vkarpenko <valeriia.karpenko@billing.ru>
2021-02-18 11:09:04 +0300

    add lxml adaptor

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import allure
 from model.group import Group
 
 
@@ -6,13 +7,15 @@ def test_add_group(app, db, json_groups, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' %group):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
